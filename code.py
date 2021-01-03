a4e96a3d86a69c2e07d3ad4471a7ec3c9984d98d
Sai Nikhil Gona <67323988+Nikhil619@users.noreply.github.com>
2021-01-03 22:04:54 +0530

    Update code.py

--- code.py
+++ code.py
@@ -19,24 +19,34 @@ vehicle=connect(args.connect, baud=57600,wait_ready=True )
 
 def arm_and_takeoff(aTargetAltitude):
 
-  print "Basic pre-arm checks"
+  print ("Basic pre-arm checks")
   # Don't let the user try to arm until autopilot is ready
   while not vehicle.is_armable:
-    print " Waiting for vehicle to initialise..."
+    print (" Waiting for vehicle to initialise...")
     time.sleep(1)
         
-  print "Arming motors"
+  print ("Arming motors")
   # Copter should arm in GUIDED mode
   vehicle.mode    = VehicleMode("GUIDED")
   vehicle.armed   = True
 
   while not vehicle.armed:
-    print " Waiting for arming..."
+    print (" Waiting for arming...")
     time.sleep(1)
 
   print "Taking off!"
   arm_and_takeoff(1) # Take off to target altitude
+  vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
 
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+    #  after Vehicle.simple_takeoff will execute immediately).
+  while True:
+      print (" Altitude: ", vehicle.location.global_relative_frame.alt)
+      #Break and return from function just below target altitude.
+      if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+          print ("Reached target altitude")
+          break
+      time.sleep(1)
 
 cap = cv2.VideoCapture(0)
 
